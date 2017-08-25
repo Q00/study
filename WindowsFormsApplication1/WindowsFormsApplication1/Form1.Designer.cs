eee1e5f6becb55db1353c81dcecb37a839bdcd74
Lee Jae Gyu <ljk950327@gmail.com>
2017-08-25 15:09:35 +0900

    네이버에서 실시간 검색어 뽑아오기

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.databox = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(531, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(412, 501);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // databox
+            // 
+            this.databox.Location = new System.Drawing.Point(37, 53);
+            this.databox.Multiline = true;
+            this.databox.Name = "databox";
+            this.databox.Size = new System.Drawing.Size(465, 501);
+            this.databox.TabIndex = 1;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(64, 13);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 2;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 575);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.databox);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox databox;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        String path = "https://www.naver.com";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.AbsoluteUri == webBrowser1.Url.AbsoluteUri)
+            {
+                string data = "";
+                HtmlDocument Doc = webBrowser1.Document;
+                HtmlElementCollection elems = Doc.GetElementsByTagName("div");
+                foreach (HtmlElement elem in elems)
+                {
+                    if (elem.GetAttribute("className") == "ah_roll_area PM_CL_realtimeKeyword_rolling")
+                    {
+                        for (int i = 0; i < elem.GetElementsByTagName("span").Count; i++)
+                        {
+
+                            if (elem.GetElementsByTagName("span")[i].GetAttribute("className") == "ah_r")
+                            {
+                                data = elem.GetElementsByTagName("span")[i].InnerText;
+                                databox.AppendText(data + "\n");
+                            }
+                            if (elem.GetElementsByTagName("span")[i].GetAttribute("className") == "ah_k")
+                            {
+                                data = elem.GetElementsByTagName("span")[i].InnerText;
+                                databox.AppendText(data + "\n");
+
+                            }
+
+                        }
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(path);
+        }
+    }
+}
+
